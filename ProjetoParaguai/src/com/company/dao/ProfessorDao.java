ce44e3debbd019432fb7ab06bdc9ac21c48a270d
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2020-12-01 15:02:43 -0300

    segundo commit

---
+++ ProjetoParaguai/src/com/company/dao/ProfessorDao.java
@@ -0,0 +1,29 @@
+package com.company.dao;
+
+import com.company.model.Aluno;
+import com.company.model.Prof;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProfessorDao {
+
+    Connection con = new ConnectionFactory().getConnection();
+
+    public ProfessorDao() {
+    }
+
+    public void inserirDado(Prof prof) throws SQLException {
+        String sql = "insert into Emds.Professor" +
+                "(cod, nome, email)" +
+                "values(?,?,?)";
+        PreparedStatement stmt = this.con.prepareStatement(sql);
+        stmt.setInt(1, prof.getCod());
+        stmt.setString(2, prof.getNome());
+        stmt.setString(3, prof.getEmail());
+
+        stmt.execute();
+        stmt.close();
+    }
+}
